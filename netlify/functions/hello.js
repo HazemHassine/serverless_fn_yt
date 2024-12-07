ac59f460f3ddc4ca87b31b5d0095667ed36717e9
HazemHassine <hazemhassine.edu@gmail.com>
2024-12-07 18:10:55 +0100

    testing

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,7 @@
 const { YoutubeTranscript } = require('youtube-transcript');
 
 exports.handler = async (event, context) => {
-    const url = "https://www.youtube.com/watch?v=V4Z8EdiJxgk";
+    const url = "https://www.youtube.com/watch?v=BmUZ2wp1lM8&pp=ygUKa3Vyemdlc2FndA%3D%3D";
     const transcript = await YoutubeTranscript.fetchTranscript(url, { lang: "en" });
     return {
         statusCode: 200,
